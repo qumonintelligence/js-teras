beeec784a8a79be07e2b09c5251a2bb95dd80bd5
seb <seb@example.com>
2021-12-27 16:16:52 +0800

    Added redux saga

--- packages/teras-core/src/index.ts
+++ packages/teras-core/src/index.ts
@@ -1,6 +1,6 @@
 import * as rr from 'react-redux';
+import * as reduxSaga from 'redux-saga';
 import Store from './store';
 import connectSocket from './socket';
 
-export { rr as default, Store, connectSocket };
-
+export { rr as default, Store, connectSocket, reduxSaga };
